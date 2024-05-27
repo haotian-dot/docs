9099f291ab863227e5bf9064a466ed400a63654d
Woodii <33593572+Woodii1998@users.noreply.github.com>
2024-05-27 13:29:27 +0800

    update: add openapi link (#89)
    * update: add openapi link
    
    * update labels
    
    ---------
    
    Co-authored-by: Yujing Zheng <yujing@coscene.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,6 +95,11 @@ const config = {
           },
           { href: 'https://www.coscene.cn', label: '官方网站', position: 'right' },
           { href: 'https://github.com/coscene-io', label: 'GitHub', position: 'right' },
+          {
+            href: 'https://petstore.swagger.io/?url=https://download.coscene.cn/openapi/openapi.yaml',
+            label: 'OpenAPI',
+            position: 'right',
+          },
         ],
       },
       footer: {
